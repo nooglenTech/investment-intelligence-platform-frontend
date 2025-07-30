f263672f91bf2ddc954daa83ae10288fa8df42e9
nooglenTech <jetnuygen06@gmail.com>
2025-07-30 09:54:19 -0500

    vercel v6

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,14 @@
+// src/middleware.ts
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
 export default clerkMiddleware({
-  // Add the sign-in and sign-up pages to the list of public routes
-  // so that users can access them without being logged in.
   publicRoutes: ['/sign-in', '/sign-up'],
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and static files
     '/((?!.*\\..*|_next).*)',
-    // Run middleware on all routes except static files
     '/',
-    // Run middleware on API routes
     '/(api|trpc)(.*)',
   ],
 };
-
